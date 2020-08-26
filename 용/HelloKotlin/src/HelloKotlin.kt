ce0bd5383f0bb4d8655e727618c9e10185c7b933
Your Name <you@example.com>
2020-08-26 18:31:16 +0900

    1

---
+++ 용/HelloKotlin/src/HelloKotlin.kt
@@ -0,0 +1,10 @@
+fun main(args : Array<String>){
+	println("Hello, Kotlin!\n")
+	
+	println(sum(2,3))
+}
+
+
+fun sum(a:Int,b:Int):Int{
+	return a+b
+}
---
+++ 용/instagram/app/src/main/java/kr/sqlitedb/instagram/LoginActivity.kt
@@ -0,0 +1,16 @@
+package kr.sqlitedb.instagram
+
+import android.graphics.drawable.Drawable
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
