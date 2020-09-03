f2335b565d81140271172cb86078204e1bbcafad
gahee <ga_hee3230@naver.com>
2020-09-03 13:59:06 +0900

    commit

--- 이가희/js/side.js
+++ 이가희/js/side.js
@@ -4,10 +4,10 @@ let isFollow = [false,false,false,false,false,];
 for(let x = 0; x < 5; x++){
   followBtn[x].addEventListener("click", () => {
     if(!isFollow[x]){
-      followBtn[x].innerHTML = '팔로잉';
+      followBtn[x].innerHTML = "팔로잉";
       isFollow[x] = true;
     } else {
-      followBtn[x].innerHTML = '팔로우';
+      followBtn[x].innerHTML = "팔로우";
       isFollow[x] = false;
     }
   });
