f7a52c9250fdfbd29bde4efd76e8a795577e6d90
JJongTaeng <jjojjo141995@gmail.com>
2020-10-06 20:29:36 +0900

    branch push

--- WEB/mainFolder/public/js/feedFront.js
+++ WEB/mainFolder/public/js/feedFront.js
@@ -45,11 +45,15 @@
             const likesAxios = await axios.post('/feed_like_process', {postID: feedItem.dataset.post_id});
             const likesAxiosData = await likesAxios.data;
             console.log(likesAxiosData)
-            if(likesAxiosData.data1.length!==0){
-                likeButton.setAttribute('fill', '#ed4956')
+			if (likesAxiosData.data1.length !== 0) {
+				likeButton.setAttribute('fill', '#ed4956')
                 likeButton.children[0].setAttribute('d', "M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z");
                 likeButton.nextElementSibling.innerHTML = `${likesAxiosData.data2.length}명`;
-            }
+			} else {
+                likeButton.setAttribute('fill', '#262626')
+                likeButton.firstElementChild.setAttribute('d', "M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z");
+                likeButton.nextElementSibling.innerHTML = `${likesAxiosData.data2.length}명`;
+			}
             for (let i = 0; i < commentAxiosData.length; i++) {
                 const commentList = document.createElement('li');
                 const commentImage = document.createElement('a');
@@ -85,6 +89,7 @@
                 feedSlideList.appendChild(feedImageItems);
                 feedImageItems.style.backgroundImage = `url('../data/${searchAxiosData.post[feedItem.dataset.number].post_id}/${searchAxiosData.images[feedItem.dataset.number][i]}')`
             }
+            feedPostModalContainer.style.top = 0;
             feedPostModalContainer.style.opacity = '1';
             feedPostModalContainer.style.zIndex = 20;
 
@@ -169,6 +174,7 @@
         } else if (slideBox) {
         } else if (modalBox) {
             modalBox.style.opacity = '0';
+            feedPostModalContainer.style.top = '4rem';
             feedPostModalContainer.style.zIndex = 0;
             for (let i = 0; i < feedSlideItems.length; i++) {
 
